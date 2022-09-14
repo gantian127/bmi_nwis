feb5f5593bbf4fc103aaeb10051465523136f446
gantian127 <jamy127@foxmail.com>
2022-09-14 16:50:52 -0600

    update versioneer

--- bmi_nwis/_version.py
+++ bmi_nwis/_version.py
@@ -23,9 +23,9 @@ def get_keywords():
     # setup.py/versioneer.py will grep for the variable names, so they must
     # each be defined on a line of their own. _version.py will just call
     # get_keywords().
-    git_refnames = " (HEAD -> master)"
-    git_full = "766710de3933a76e997547d1769ee55ca4fb9e2c"
-    git_date = "2022-09-14 15:09:05 -0600"
+    git_refnames = "$Format:%d$"
+    git_full = "$Format:%H$"
+    git_date = "$Format:%ci$"
     keywords = {"refnames": git_refnames, "full": git_full, "date": git_date}
     return keywords
 
