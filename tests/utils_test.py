f11b6255bf9a13ceb2a524a228282367e33098ff
gantian127 <jamy127@foxmail.com>
2024-01-25 16:10:10 -0700

    update unit test

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -1,41 +1,58 @@
-import pytest
+from __future__ import annotations
+
 import os
 
+import pytest
 from bmi_nwis import NwisData
 
 
 # test user input for get_data() method
 
+
 # test sites
 def test_sites():
     with pytest.raises(ValueError):
-        NwisData().get_data(sites='invalid_site', start='2022-01-01', end='2022-01-03', service='iv')
+        NwisData().get_data(
+            sites="invalid_site", start="2022-01-01", end="2022-01-03", service="iv"
+        )
 
     with pytest.raises(ValueError):
-        NwisData().get_data(sites='033390001', start='2022-01-01', end='2022-01-03', service='iv')
+        NwisData().get_data(
+            sites="033390001", start="2022-01-01", end="2022-01-03", service="iv"
+        )
 
 
 # test service
 def test_service():
     with pytest.raises(ValueError):
-        NwisData().get_data(sites='03339000', start='2022-01-01', end='2022-01-03', service='invalid_type')
+        NwisData().get_data(
+            sites="03339000",
+            start="2022-01-01",
+            end="2022-01-03",
+            service="invalid_type",
+        )
 
 
 # test output
 def test_output_invalid_dir(tmpdir):
     with pytest.raises(ValueError):
-        NwisData().get_data(sites='03339000', start='2022-01-01', end='2022-01-03', service='iv',
-                        output=os.path.join(tmpdir, 'error'))
+        NwisData().get_data(
+            sites="03339000",
+            start="2022-01-01",
+            end="2022-01-03",
+            service="iv",
+            output=os.path.join(tmpdir, "error"),
+        )
 
 
 @pytest.mark.filterwarnings("ignore:numpy.ndarray size changed")
 def test_output_valid_dir(tmpdir):
-    NwisData().get_data(sites='03339000', start='2022-01-01', end='2022-01-03', service='iv',
-                    output=os.path.join(tmpdir, 'test.nc'))
+    NwisData().get_data(
+        sites="03339000",
+        start="2022-01-01",
+        end="2022-01-03",
+        service="iv",
+        output=os.path.join(tmpdir, "test.nc"),
+    )
 
     assert len(os.listdir(tmpdir)) == 1
-
-
-
-
-
